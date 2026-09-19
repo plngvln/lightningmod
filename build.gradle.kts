49cad4922e560917405f42f6cb825ef043c28638
maksim <msakunov@inbox.ru>
2026-09-19 21:38:29 +0300

    port to 26.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "2.3.0"
-    id("net.fabricmc.fabric-loom") version "1.15-SNAPSHOT"
+    id("net.fabricmc.fabric-loom") version "1.17.20"
     id("maven-publish")
 }
 
